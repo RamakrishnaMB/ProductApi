c76eb95e4e8ffbffee1844b61165dd26929b4af5
ramakrishna <bhaskar-ramakrishna.mandakalli@external.thalesgroup.com>
2025-03-17 13:01:39 +0800

    CRUD Sample for React using .Net 8

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("ProductDB"));
+builder.Services.AddCors(opt => opt.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+app.Run();
+
